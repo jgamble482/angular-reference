4e9ee0ca5a32e04f30da6284c75ba7967dabee67
Jordan Gamble <jgamble482@gmail.com>
2021-02-14 21:19:25 -0500

    App can display users from back-end

--- dating-app/src/app/app.component.ts
+++ dating-app/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { UserService } from './user.service';
+import { UserService } from './services/user.service';
+import {User} from './models/user'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'dating-app';
+  users: User[];
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService){
+   this.userService.getAll().subscribe(data => {
+     this.users = data
+   });
+    
+  }
+
+
+
 }
---
+++ dating-app/src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {User} from '../models/user'
+import { Observable } from 'rxjs';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<User[]>{
+    return this.http.get<User[]>(`${environment.apiUrl}/User`);
+  }
+}
