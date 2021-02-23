b83988067ddb5c63453f26ddb1e2096cc2c6ad5d
Jordan Gamble <jgamble482@gmail.com>
2021-02-23 18:31:12 -0500

    Added a jwt interceptor to retrieve token and then send token in subsequent requests

--- dating-app/src/app/services/member.service.ts
+++ dating-app/src/app/services/member.service.ts
@@ -3,11 +3,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {Member} from '../models/member';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +14,10 @@ export class MemberService {
   constructor(private http: HttpClient) { }
 
   getAll(){
-    return this.http.get<Member[]>(`${environment.apiUrl}/user`, httpOptions);
+    return this.http.get<Member[]>(`${environment.apiUrl}/user`);
   }
 
   getMember(username: string){
-    return this.http.get<Member[]>(`${environment.apiUrl}/user/${username}`, httpOptions);
+    return this.http.get<Member[]>(`${environment.apiUrl}/user/${username}`);
   }
 }
